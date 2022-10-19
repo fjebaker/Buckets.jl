669a49ba44724a256e9e8d8e874f0e165a4813ec
Fergus Baker <fergusbkr@gmail.com>
2022-10-19 13:55:16 +0100

    Down sample fix (#2)
    * remove parallel (race conditions) and fix down sample
    
    * down sample test
    
    * bump version
    
    * julia formatting

--- src/Buckets.jl
+++ src/Buckets.jl
@@ -1,14 +1,17 @@
 module Buckets
 
 using Statistics
-using Base.Threads: @threads 
+
+export mean
+
+using Base.Threads: @threads
 
 abstract type AbstractBucketAlgorithm end
 struct Simple <: AbstractBucketAlgorithm end
 struct DownSample <: AbstractBucketAlgorithm end
 
 @inline function find_bin_index(x, bins)
-    searchsortedfirst(bins, x) 
+    searchsortedfirst(bins, x)
 end
 
 @inline function find_bin_index(x, bins, last_bin)
@@ -16,43 +19,53 @@ end
     min(_bin_low, last_bin)
 end
 
+function _check_X_y_delta(X, y, Δ)
+    if length(X) != length(y) + Δ
+        throw(
+            DimensionMismatch(
+                "For X with $(length(X)) elements, y must have $(length(X) - Δ) elements.",
+            ),
+        )
+    end
+end
+
 function _check_X_y(X, y)
     if size(X) != size(y)
-        error("Dimensions mismatch for X and y.")
+        throw(DimensionMismatch("Dimensions mismatch for X and y."))
     end
 end
 
 function _check_X_y(X1, X2, y::AbstractVector)
     if (size(X1) != size(X2)) || (size(X1) != size(y))
-        error("Dimensions mismatch for X1, X2 and y.")
+        throw(DimensionMismatch("Dimensions mismatch for X1, X2 and y."))
     end
-end 
+end
 
 function _check_X_y(X1, X2, y::AbstractMatrix)
     if (size(X1, 1), size(X2, 1)) != size(y)
-        error("Dimensions mismatch for X1, X2 and y.")
+        throw(DimensionMismatch("Dimensions mismatch for X1, X2 and y."))
     end
-end 
+end
 
 function _check_bin_output_args(output, noutput, bins1, bins2)
     _check_sorted(bins1)
     _check_sorted(bins2)
     if ((size(bins1, 1), size(bins2, 1)) != size(output)) || size(output) != size(noutput)
-        error("Dimension mistmatch between output arrays and bins.")
+        throw(DimensionMismatch("Dimension mistmatch between output arrays and bins."))
     end
 end
 
 function _check_bin_output_args(output, noutput, bins)
     _check_sorted(bins)
     if (size(output) != size(bins)) || (size(output) != size(noutput))
-        error("Dimension mistmatch between output arrays and bins.")
+        throw(DimensionMismatch("Dimension mistmatch between output arrays and bins."))
     end
 end
 
 function _check_bin_output_args(output, bins)
     _check_sorted(bins)
     if (size(output) != size(bins))
-        error("Dimension mistmatch between output arrays and bins.")
+        throw(DimensionMismatch("Dimension mistmatch between output arrays and bins."))
     end
 end
 
@@ -68,24 +81,34 @@ end
 
 function _check_sorted(X)
     if !issorted(X)
-        error("Input `X` is not sorted. `X` and `y` should be sorted together (e.g. with `sortperm`).")
+        error(
+            "Input `X` is not sorted. `X` and `y` should be sorted together (e.g. with `sortperm`).",
+        )
     end
 end
 
-_apply_reduction!(output, _, ::typeof(sum)) = 
-    output
+_apply_reduction!(output, _, ::typeof(sum)) = output
 
-_apply_reduction!(output, noutput, ::typeof(mean)) =
-    @. output = output / noutput
+_apply_reduction!(output, noutput, ::typeof(mean)) = @. output = output / noutput
 
 # Algorithms
 
-function bucket!(::Simple, output, noutput, X1, X2, y::AbstractMatrix, bins1, bins2; reduction=mean)
+function bucket!(
+    ::Simple,
+    output,
+    noutput,
+    X1,
+    X2,
+    y::AbstractMatrix,
+    bins1,
+    bins2;
+    reduction = sum,
+)
     _check_bin_args(output, noutput, X1, X2, y, bins1, bins2)
     last_bin1 = lastindex(bins1)
     last_bin2 = lastindex(bins2)
 
-    @fastmath @inbounds @threads for i in eachindex(X1)
+    for i in eachindex(X1)
         bin_row = find_bin_index(X1[i], bins1, last_bin1)
         for j in eachindex(X2)
             bin_column = find_bin_index(X2[j], bins2, last_bin2)
@@ -93,18 +116,28 @@ function bucket!(::Simple, output, noutput, X1, X2, y::AbstractMatrix, bins1, bi
             output[bin_column, bin_row] += y[bin_column, bin_row]
             noutput[bin_column, bin_row] += 1
         end
-    end  
+    end
     _apply_reduction!(output, noutput, reduction)
     output
 end
 
-function bucket!(::Simple, output, noutput, X1, X2, y::AbstractVector, bins1, bins2; reduction=mean)
+function bucket!(
+    ::Simple,
+    output,
+    noutput,
+    X1,
+    X2,
+    y::AbstractVector,
+    bins1,
+    bins2;
+    reduction = sum,
+)
     _check_bin_args(output, noutput, X1, X2, y, bins1, bins2)
 
     last_bin1 = lastindex(bins1)
     last_bin2 = lastindex(bins2)
 
-    @fastmath @inbounds @threads for i in eachindex(X1)
+    for i in eachindex(X1)
         bin_column = find_bin_index(X1[i], bins1, last_bin1)
         bin_row = find_bin_index(X2[i], bins2, last_bin2)
 
@@ -116,11 +149,11 @@ function bucket!(::Simple, output, noutput, X1, X2, y::AbstractVector, bins1, bi
     output
 end
 
-function bucket!(::Simple, output, noutput, X, y, bins; reduction=mean)
+function bucket!(::Simple, output, noutput, X, y, bins; reduction = sum)
     _check_bin_args(output, noutput, X, y, bins)
     last_bin = lastindex(bins)
 
-    @fastmath @inbounds @threads for i in eachindex(X)
+    for i in eachindex(X)
         bin_index = find_bin_index(X[i], bins, last_bin)
 
         output[bin_index] += y[i]
@@ -134,25 +167,25 @@ end
 """
     bucket!(::DownSample, output, X, y, bins)
 
-Down-sample the values in `y` in current bins `X` to fewer bins in `bins`.
+Down-sample the values in `y` in current bins `X` to fewer bins in `bins` by linearly
+splitting bins where they overlap a new bin edge.
 ```
-            x₁   Δx   x₂        x₃
+            x₁   Δx   x₂        
   |         |---Δx----|         |
- *                *                *
+ .                .                .
             |--B--|-C-|
 ```
 
 ```math
 \\gamma = \\min \\left( \\frac{B}{\\Delta x}, 1 \\right)
 ```
-
 """
 function bucket!(::DownSample, output, X, y, bins)
-    _check_X_y(X, y)
+    _check_X_y_delta(X, y, 1)
     _check_bin_output_args(output, bins)
     _check_sorted(X)
     # additionally need to make sure fewer bins than X
-    if length(X) > length(bins)
+    if length(X) < length(bins)
         error("DownSample requires length of bins to be less than or equal to length of X.")
     end
 
@@ -166,8 +199,8 @@ function bucket!(::DownSample, output, X, y, bins)
     end
 
     # special case for first bin
-    @inbounds if start > 1
-        x₁= X[start-1]
+    if start > 1
+        x₁ = X[start-1]
         x₂ = X[start]
         b₁ = bins[1]
 
@@ -175,28 +208,30 @@ function bucket!(::DownSample, output, X, y, bins)
         Δx = x₂ - x₁
         Δb = x₂ - b₁
         γ = min(Δb / Δx, 1)
-        output[1] = y[start-1] * (1 - γ)
+        output[1] += y[start-1] * (1 - γ)
     end
 
-    @fastmath @inbounds @threads for i in start:(last_x-1)
-        x₁= X[i]
+    for i = start:(last_x-1)
+        x₁ = X[i]
         x₂ = X[i+1]
 
         j = find_bin_index(x₁, bins)
-        if j+1 > last_bin
+        if j > last_bin
             break
         end
 
-        b₂ = bins[j+1]
+        b₂ = bins[j]
 
         # find ratio of lengths
         Δx = x₂ - x₁
         Δb = b₂ - x₁
         γ = min(Δb / Δx, 1)
 
-        output[j] = y[i] * γ
-        # carry over
-        output[j+1] = y[i+1] * (1 - γ) 
+        output[j] = y[i] * γ + output[j]
+        # carry over if not at end
+        if j + 1 <= last_bin
+            output[j+1] = y[i] * (1 - γ) + output[j+1]
+        end
     end
     output
 end
@@ -208,13 +243,27 @@ function allocate_output(::DownSample, X, y::AbstractArray{T}, bins) where {T}
     (output,)
 end
 
-function allocate_output(::AbstractBucketAlgorithm, X, y::AbstractArray{T}, bins; kwargs...) where {T}
+function allocate_output(
+    ::AbstractBucketAlgorithm,
+    X,
+    y::AbstractArray{T},
+    bins;
+    kwargs...,
+) where {T}
     output = zeros(T, length(bins))
     noutput = similar(output)
     (output, noutput)
 end
 
-function allocate_output(::AbstractBucketAlgorithm, X1, X2, y::AbstractArray{T}, bins1, bins2; kwargs...) where {T}
+function allocate_output(
+    ::AbstractBucketAlgorithm,
+    X1,
+    X2,
+    y::AbstractArray{T},
+    bins1,
+    bins2;
+    kwargs...,
+) where {T}
     output = zeros(T, (length(bins1), length(bins2)))
     noutput = similar(output)
     (output, noutput)
@@ -222,13 +271,10 @@ end
 
 # Allocating interface
 
-function bucket(
-    alg::AbstractBucketAlgorithm,
-    args...; kwargs...
-)
+function bucket(alg::AbstractBucketAlgorithm, args...; kwargs...)
     allocated_outputs = allocate_output(alg, args...; kwargs...)
     bucket!(alg, allocated_outputs..., args...; kwargs...)
-end 
+end
 
 """
     bucket(X, y, bins; kwargs...)
@@ -254,8 +300,9 @@ Two dimensional contiguous binning, where `y` can either be
 and `bins1` (`bins2`) the bin edges for `X1` (`X2`).
 - `AbstractVector`: `X1` and `X2` are effectively the coordinates of `y`
 """
-bucket(X1, X2, y, bins1, bins2; kwargs...) = bucket(Simple(), X1, X2, y, bins1, bins2; kwargs...)
+bucket(X1, X2, y, bins1, bins2; kwargs...) =
+    bucket(Simple(), X1, X2, y, bins1, bins2; kwargs...)
 
-export bucket, bucket!, Simple, AbstractBucketAlgorithm
+export bucket, bucket!, Simple, DownSample, AbstractBucketAlgorithm
 
 end # module Buckets
--- test/runtests.jl
+++ test/runtests.jl
@@ -6,10 +6,32 @@ using Buckets
     X = collect(range(1.0, 10.0, 20))
     y = ones(Float64, length(X))
     bins = 1:10
-    y_binned = bucket(X, y, bins; reduction=sum)
+    y_binned = bucket(X, y, bins; reduction = sum)
     @test y_binned == [1.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 3.0]
-    y_binned = bucket(X, y, bins)
+    y_binned = bucket(X, y, bins; reduction = mean)
     @test y_binned == [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
+end
+
+@testset "down-sample" begin
+    # generate known data
+    X = collect(range(1.0, 10.0, 20))
+    y = ones(Float64, length(X) - 1)
+    bins = 1:1.5:11.5
+    y_binned = bucket(DownSample(), X, y, bins)
+    @test y_binned == [
+        0.0,
+        3.1666666666666665,
+        3.166666666666667,
+        3.166666666666667,
+        3.1666666666666665,
+        3.166666666666666,
+        3.1666666666666674,
+        0.0,
+    ]
+    @test sum(y_binned) == sum(y)
+
+    y = ones(Float64, 20)
+    @test_throws DimensionMismatch bucket(DownSample(), X, y, bins)
 end
 
 # little bit of aqua
